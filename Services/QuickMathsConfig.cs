5a851e4ea92f70275e4feb816fe7d69283f5ba6f
Sam Aripov <169399701+samaripov@users.noreply.github.com>
2026-01-10 12:27:34 -0500

    Initial design of the interface.

---
+++ Services/QuickMathsConfig.cs
@@ -0,0 +1,6 @@
+public class QuickMathsConfig
+{
+    public List<int> numbersToFocusOn { get; set; } = new List<int>();
+    public string difficulty {get; set;} = "normal";
+    public int roundDurationInSeconds { get; set; }= 300;
+}
